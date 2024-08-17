39139f4fee66e6bc214fcf39c25e7e6ccd5d9596
Tausif Ahmad <tausifahmad754@gmail.com>
2024-08-17 21:39:35 +0530

    Types of constructor

--- OOPS.java
+++ OOPS.java
@@ -18,15 +18,30 @@ class Student{
         System.out.println(this.name);
         System.out.println(this.age);
     }
+// Parameterized constructor
+    Student(String name,int age){
+        // System.out.println("Constructor called");
+        this.name=name;
+        this.age=age;
+    }
+// copy Constructor
+    Student(Student s2){
+        this.name=s2.name;
+        this.age=s2.age;
+
+    }
 }
 
 
 public class OOPS{
     public static void main(String args[]){
-        Student s=new Student();
+        Student s=new Student("Tausif",22);
         s.name="Tausif";
         s.age=22;
-        s.studentInfo();
+        // s.studentInfo();
+
+        Student s2=new Student(s);
+        s2.studentInfo();
 
     }
 
